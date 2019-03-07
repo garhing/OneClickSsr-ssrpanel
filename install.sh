06e4418d0abba189806de6969d6c0a6c8cefe564
garhing <37818717+garhing@users.noreply.github.com>
2019-03-07 21:39:07 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -27,10 +27,10 @@ yum remove nc -y >/dev/null 2>&1
 yum install git -y >/dev/null 2>&1
 easy_install pip >/dev/null 2>&1
 cd /root
-wget https://raw.githubusercontent.com/mzfqy/OneClickSsr-ssrpanel/master/cron >/dev/null 2>&1
-wget https://raw.githubusercontent.com/mzfqy/OneClickSsr-ssrpanel/master/ssr >/dev/null 2>&1
-wget https://raw.githubusercontent.com/mzfqy/OneClickSsr-ssrpanel/master/ncat-7.60-1.x86_64.rpm>/dev/null 2>&1
-wget https://raw.githubusercontent.com/mzfqy/OneClickSsr-ssrpanel/master/restartssr>/dev/null 2>&1
+wget https://raw.githubusercontent.com/garhing/OneClickSsr-ssrpanel/master/cron >/dev/null 2>&1
+wget https://raw.githubusercontent.com/garhing/OneClickSsr-ssrpanel/master/ssr >/dev/null 2>&1
+wget https://raw.githubusercontent.com/garhing/OneClickSsr-ssrpanel/master/ncat-7.60-1.x86_64.rpm>/dev/null 2>&1
+wget https://raw.githubusercontent.com/garhing/OneClickSsr-ssrpanel/master/restartssr>/dev/null 2>&1
 rpm -ivh ncat-7.60-1.x86_64.rpm >/dev/null 2>&1
 ln -s /usr/bin/ncat /bin/nc >/dev/null 2>&1
 cd /root
@@ -47,7 +47,7 @@ echo /usr/local/lib > /etc/ld.so.conf.d/usr_local_lib.conf
 ldconfig
 echo 正在配置ssr
 cd /root
-git clone https://github.com/mzfqy/shadowsocksr >/dev/null 2>&1
+git clone https://github.com/garhing/shadowsocksr >/dev/null 2>&1
 chmod 777 * -R
 mv ssr /bin/ssr
 mv restartssr /bin/restartssr
